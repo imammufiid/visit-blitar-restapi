d435e53fabe3ff56575044124686f025de63e867
imufiid <imufiid98@gmail.com>
2021-01-22 14:55:53 +0700

    get all tourist attraction

--- app/Http/Controllers/TourismController.php
+++ app/Http/Controllers/TourismController.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tourism;
 use Symfony\Component\HttpFoundation\Response;
 
 class TourismController extends Controller
 {
 
+   public function get_all_tourism()
+   {
+      $tourism = Tourism::all();
+
+      if($tourism != null) {
+         return response()->json([
+            'status' => Response::HTTP_OK,
+            'message' => "This data",
+            'data' => $tourism
+         ]);
+      } else {
+         return response()->json([
+            'status' => Response::HTTP_NOT_FOUND,
+            'message' => "Data Not Available",
+         ]);
+      }
+   }
+
+   
 
 }
