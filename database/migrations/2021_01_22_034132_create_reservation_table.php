13647182d523df49955a0776a13fefd241b621e1
imufiid <imufiid98@gmail.com>
2021-01-22 14:07:32 +0700

    create reservation migration

---
+++ database/migrations/2021_01_22_034132_create_reservation_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 100);
+            $table->unsignedBigInteger("tourism_id");
+            $table->string("date");
+            $table->integer("total_visitors");
+            $table->timestamps();
+
+            $table->foreign("tourism_id")->references("id")->on("tourisms");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
